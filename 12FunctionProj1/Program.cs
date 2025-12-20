8c75f053d8c7c6d3865729ba40ad75eff2d30f19
Fati.Z <Fatemezabihi1378@gmail.com>
2025-12-20 08:06:10 -0800

    Add files via upload

---
+++ 12FunctionProj1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        int indent = 0;
+        bool increasing = true;
+
+        while (true)
+        { 
+            Console.WriteLine(new string(' ' , indent) + "*");
+            Thread.Sleep(100);
+
+
+            if (increasing)
+            {
+                indent++;
+                if (indent == 20)
+                    increasing = false;
+            }
+            else
+            {
+              indent--;
+              if (indent == 0)
+                  increasing = true;
+            }
+        } 
+    }
+}
---
+++ 13FunctionProj2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            while (true) // Main program loop
+            {
+                // Draw lines with increasing length
+                for (int i = 1; i <= 8; i++)
+                {
+                    Console.WriteLine(new string('-', i * i));
+                    Thread.Sleep(100);
+                }
+
+                // Draw lines with decreasing length
+                for (int i = 7; i >= 2; i--)
+                {
+                    Console.WriteLine(new string('-', i * i));
+                    Thread.Sleep(100);
+                }
+            }
+        }
+        catch (Exception)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
