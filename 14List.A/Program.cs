b16b3e8818630fe25a43b2a544d5aa25ece8ad10
Fati.Z <Fatemezabihi1378@gmail.com>
2025-12-24 00:50:31 -0800

    Add files via upload

---
+++ 14List.A/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> catNames = new List<string>();
+
+        while (true)
+        {
+            Console.WriteLine(
+                "Enter the name of cat " + (catNames.Count + 1) +
+                " (Or enter nothing to stop.):"
+            );
+
+            string name = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(name))
+                break;
+
+            // List concatenation equivalent
+            catNames.Add(name);
+        }
+
+        Console.WriteLine("The cat names are:");
+        foreach (string name in catNames)
+        {
+            Console.WriteLine(" " + name);
+        }
+    }
+}
+
