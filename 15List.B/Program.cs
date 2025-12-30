d4ec5b3e846d1b003c3e58c555919e7de4c83a82
Fati.Z <Fatemezabihi1378@gmail.com>
2025-12-30 11:24:12 -0800

    Add files via upload

---
+++ 15List.B/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> people = new List<string>
+        {
+            "Alice", "Bob", "Carol", "David"
+        };
+
+        Random random = new Random();
+
+        for (int i = people.Count - 1; i > 0; i--)
+        {
+            int j = random.Next(i + 1);
+
+            // swap people[i] and people[j]
+            string temp = people[i];
+            people[i] = people[j];
+            people[j] = temp;
+        }
+
+        foreach (string person in people)
+        {
+            Console.WriteLine(person);
+        }
+    }
+}
